662134a3875840e202a1e622f00f5f9c6107bde5
Alireza Aberoumand <maya7192@yahoo.com>
2025-05-26 04:17:05 -0700

    Stagger training

--- src/app.js
+++ src/app.js
@@ -10,15 +10,25 @@
 // })
 
 
-gsap.from('#text1', {
-  x: 1000,
-  y: 500,
-  backgroundColor: '#9ACD32',
-  borderRadius: '50%',
-  scale: 1.5,
-  rotate: 360,
-  duration: 2,
-  delay: 1,
-  repeat: -1,
-  yoyo: true
-})
+// gsap.from('#text1', {
+//   x: 1000,
+//   y: 500,
+//   backgroundColor: '#9ACD32',
+//   borderRadius: '50%',
+//   scale: 1.5,
+//   rotate: 360,
+//   duration: 2,
+//   delay: 1,
+//   repeat: -1,
+//   yoyo: true
+// })
+
+gsap.from(
+    '#text1',
+    {
+        x: 1000,
+        duration: 2,
+        stagger: 2,
+        repeat: -1
+    }
+)
