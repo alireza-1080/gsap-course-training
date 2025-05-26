39bc86c26d8bc3f95b4bfa1942d97f84bb608630
Alireza Aberoumand <maya7192@yahoo.com>
2025-05-26 04:12:13 -0700

    2nd Training

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
-gsap.to('#text1', {
+// gsap.to('#text1', {
+//   x: 1000,
+//   y: 500,
+//   backgroundColor: '#9ACD32',
+//   borderRadius: '50%',
+//   scale: 1.5,
+//   rotate: 360,
+//   duration: 5,
+//   delay: 1,
+// })
+
+
+gsap.from('#text1', {
   x: 1000,
   y: 500,
   backgroundColor: '#9ACD32',
   borderRadius: '50%',
   scale: 1.5,
+  rotate: 360,
   duration: 2,
   delay: 1,
+  repeat: -1,
+  yoyo: true
 })
